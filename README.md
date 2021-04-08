11b60b744ffec34be638b92e616b5cce6e2fad9c
SherwinShen <306407158@qq.com>
2021-04-08 14:17:17 +0800

    feat: 地图部分(仅展示)

--- README.md
+++ README.md
@@ -1,28 +1,6 @@
 # 数据可视化项目-数据可视化大屏
 
-> 本项目为数据可视化简易学习项目，技术栈为 Vue + AntV
-
-
-
-## 1. 适配方案
-
-本项目选用 flexible.js 来实现自适应适配
-
-```shell
-$npm i lib-flexible -S
-```
-
-注意，本项目中需要更改一下 rem 的定义规则，在`node_modules/lib/flexible.js`中将文件将 setRemUnit 函数进行修改
-
-```js
-// set 1rem = viewWidth / 10
-function setRemUnit() {
-  var rem = docEl.clientWidth / 24;
-  docEl.style.fontSize = rem + "px";
-}
-```
-
-在项目开发过程中可以借助一些插件来将设计稿中的 px 转化为 rem，从而到达自适应的目的。
+> 本项目为数据可视化简易学习项目，技术栈为 Vue + AntV (G2plot + L7)
 
 
 
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,12 +3,73 @@
     <div class="map1"></div>
     <div class="map2"></div>
     <div class="map3"></div>
+    <div class="map-wrap">
+      <div id="map" ref="chart" class="chart"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Scene } from '@antv/l7'
+import { CountryLayer } from '@antv/l7-district'
+import { Mapbox } from '@antv/l7-maps'
+import ProvinceData from '@/data/provinceData'
+
 export default {
-  name: 'Map'
+  name: 'Map',
+  data () {
+    return {
+      scene: null
+    }
+  },
+  mounted () {
+    this.initChart()
+  },
+  methods: {
+    initChart () {
+      const scene = new Scene({
+        id: 'map',
+        // logoVisible: false,
+        map: new Mapbox({
+          pitch: 0,
+          style: 'blank',
+          zoom: 2,
+          minZoom: 2,
+          maxZoom: 3
+        })
+      })
+
+      scene.on('loaded', () => {
+        const countryLayer = new CountryLayer(scene, {
+          zIndex: 100,
+          data: ProvinceData,
+          autoFit: true,
+          joinBy: ['NAME_CHN', 'name'],
+          depth: 1,
+          provinceStroke: 'rgba(43, 196, 243, 1)',
+          cityStroke: '#EBCCB4',
+          cityStrokeWidth: 1,
+          chinaNationalStroke: 'rgba(43, 196, 243, 1)',
+          chinaNationalWidth: 1,
+          label: {
+            enable: false
+          },
+          fill: {
+            color: 'rgba(43, 196, 243, 0.42)'
+          },
+          popup: {
+            enable: true,
+            Html: props => {
+              return `<span>${props.NAME_CHN}</span>`
+            }
+          }
+        })
+        countryLayer.on('click', () => {
+          // 图层点击事件
+        })
+      })
+    }
+  }
 }
 </script>
 
